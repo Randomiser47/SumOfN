ef71dc8695861587d1c180ae73d23f65157b817a
Hassaan Alvi <110996624+Randomiser47@users.noreply.github.com>
2024-12-21 18:17:45 +0500

    Add files via upload

---
+++ SumOfN/SoN.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n = 100;
+    int i = 1;
+    int sum = 0;
+    while (i<=n)
+    {
+      sum = sum+i;
+        i++;
+       
+    }
+   cout<<"SUM IS :"<<sum<<endl;
+
+
+    return 0;
+
+}
